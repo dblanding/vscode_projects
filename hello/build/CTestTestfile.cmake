23d805709b8c3968987c997702ed6d76e5124680
Doug Blanding <dblanding@gmail.com>
2022-12-03 05:58:35 -0500

    removed /build folders

--- hello/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/doug/vscode_projects/hello
-# Build directory: /home/doug/vscode_projects/hello/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
